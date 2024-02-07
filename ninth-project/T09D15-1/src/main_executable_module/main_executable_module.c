371865274c96ec1377476d08242dd1be78ce8576
StepanErshov <127041584+StepanErshov@users.noreply.github.com>
2024-02-07 15:23:52 +0300

    Add files via upload

---
+++ ninth-project/T09D15-1/src/main_executable_module/main_executable_module.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../data_libs/data_io.h"
+#include "../data_libs/data_sort.h"
+#include "../data_libs/data_stat.h"
+#include "../data_module/data_process.h"
+#include "../yet_another_decision_module/decision.h"
+
+int main() {
+    double *data;
+    int n;
+
+    printf("LOAD DATA...\n");
+    scanf("%d", &n);
+
+    data = (double *)malloc(n * sizeof(double));
+    if (data == NULL) {
+        printf("n/a\n");
+        exit(EXIT_FAILURE);
+    }
+    input(data, n);
+
+    printf("RAW DATA:\n\t");
+    output(data, n);
+
+    printf("\nNORMALIZED DATA:\n\t");
+    normalization(data, n);
+    output(data, n);
+
+    printf("\nSORTED NORMALIZED DATA:\n\t");
+    sort(data, n);
+    output(data, n);
+
+    printf("\nFINAL DECISION:\n\t");
+    make_decision(data, n);
+    output(data, n);
+
+    free(data);
+    return 0;
+}
---
+++ ninth-project/T09D15-1/src/yet_another_decision_module/yet_another_decision_module_entry.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../data_libs/data_io.h"
+#include "../data_libs/data_stat.h"
+#include "decision.h"
+
+int main() {
+    double *data;
+    int n;
+
+    scanf("%d", &n);
+
+    data = (double *)malloc(n * sizeof(double));
+    if (data == NULL) {
+        printf("n/a\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input(data, n);
+
+    if (make_decision(data, n))
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
